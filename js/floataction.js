f39bd310298a25baaa82b0a945d44c4f8cc46f9d
d.roche <d.roche@girondenumerique.fr>
2018-06-04 16:36:18 +0200

    try to fix the floating action menu

--- js/floataction.js
+++ js/floataction.js
@@ -1,7 +1,9 @@
 function AdjustFloatAction() {
-   var ep = $('#endfiles').position();
-   var h = $(document).height();
-   if ( ep.top < h ) {
-     $('#floatactions').css('top',ep.top);   
-   }
+    var ep = $('#endfiles').position();
+    var h1 = $(document).height();
+    var g1 = h1 - ep.top;
+    // valeur 200 prise au pif,  le calcul avec la hauteur de floataction n'est pas viable !!
+    if ( g1 > 200 ) {
+	$('#floatactions').css('top',ep.top);   
+    }
 }
